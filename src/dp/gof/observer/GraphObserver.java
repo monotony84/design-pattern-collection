ba2470f6edf8db9790b713ef4e8d8899b07c8803
monotony <monotony84.d@gmail.com>
2021-04-18 23:22:32 +0900

    add observer pattern

---
+++ src/dp/gof/observer/GraphObserver.java
@@ -0,0 +1,21 @@
+package dp.gof.observer;
+
+public class GraphObserver implements Observer {
+    @Override
+    public void update(NumberGenerator numberGenerator) {
+        System.out.println("GraphObserver");
+        int count = numberGenerator.getNumber();
+
+        for (int i=0; i < count; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println("");
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
---
+++ src/dp/gof/observer/Observer.java
@@ -0,0 +1,5 @@
+package dp.gof.observer;
+
+public interface Observer {
+    public abstract void update(NumberGenerator numberGenerator);
+}
